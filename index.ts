39566cbe0a6771f929acc9a9b53f6bf8e7370244
fredericbahr <fredericbahr1@gmail.com>
2022-06-09 16:02:52 +0200

    implement individual generation

--- index.ts
+++ index.ts
@@ -1,34 +1,88 @@
-interface Individuum {
-  genotyp: string;
-  fitness: number;
+import { shuffle } from 'lodash';
+
+interface Individual {
+  genotyp: number[];
+  fitness: number | undefined;
 }
 
-const amountJobs = 3;
-const amountMaschines = 3;
+const jobAmount = 3;
+const operationAmount = 4;
 
 const ratingFunction = (genotyp: string): number => {
   return 0;
 };
 
 const shiftMutation = (
-  a: Individuum,
-  b: Individuum
-): [Individuum, Individuum] => {
+  a: Individual,
+  b: Individual
+): [Individual, Individual] => {
   return [
-    { genotyp: '', fitness: 0 },
-    { genotyp: '', fitness: 0 },
+    { genotyp: [], fitness: undefined },
+    { genotyp: [], fitness: undefined },
   ];
 };
 
-const orderRecombination = (a: Individuum, b: Individuum): Individuum => {
-  return { genotyp: '', fitness: 0 };
+const orderRecombination = (a: Individual, b: Individual): Individual => {
+  return { genotyp: [], fitness: 0 };
 };
 
-const generateSolutionIndividuals = (amount: number): Individuum[] => {
-  
+/**
+ * Generates solution individuals
+ * @amount amount of solution individuals to generate
+ *
+ * @return an array of individuals
+ */
+const generateSolutionIndividuals = (amount: number): Individual[] => {
+  const population: Individual[] = [];
+
+  for (let index = 0; index < amount; index++) {
+    population.push(generateSolutionIndividual(jobAmount, operationAmount));
+  }
+
+  return population;
+};
+
+/**
+ * Generates an individual which can be a solution to the optimization problem
+ *
+ * @retunrs an individual
+ * @see Individual
+ */
+const generateSolutionIndividual = (
+  jobAmount: number,
+  operationAmount: number
+): Individual => {
+  const sortedGenotyp = getSortedGenotyp(jobAmount, operationAmount);
+  const genotyp = shuffle(sortedGenotyp);
+  return {
+    genotyp,
+    fitness: undefined,
+  };
+};
+
+/**
+ * Generates a sorted genotype based on the jobAmount and operation/maschines amount
+ * e.g [1, 1, 1, 2, 2, 2, 3, 3, 3]
+ *
+ * @returns the genotyp of an individual
+ */
+const getSortedGenotyp = (
+  jobAmount: number,
+  operationAmount: number
+): number[] => {
+  const sortedGenotyp: number[] = [];
+
+  for (let index = 0; index < jobAmount; index++) {
+    sortedGenotyp.push(...Array(operationAmount).fill(index + 1));
+  }
+
+  return sortedGenotyp;
 };
 
 const hillclimber = () => {
   const count = 0;
   const [solutionIdividual] = generateSolutionIndividuals(1);
+  console.log(solutionIdividual.genotyp);
 };
+
+hillclimber();
