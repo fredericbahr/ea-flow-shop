ab9b1d67fbae628fdfbebd78547612fbd981a040
fredericbahr <fredericbahr1@gmail.com>
2022-06-15 21:36:14 +0200

    change constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,19 @@
 export const jobAmount = 20;
 export const operationAmount = 5;
-export const maximumIteration = 2000;
+export const maximumIteration = 500000;
 
-export const populationSize = 50;
+export const populationSize = 10;
 
 export const recombinationProbability = 0.15;
 export const mutationProbability = 0.85;
 
 export const individualSelectionCount = 30;
 export const childCreationCount = 20;
+
+/**
+ * Good values for hillclimber
+ * maximumIteration = 300000
+ *
+ * Better values for hillclimber
+ * maximumIteration = 300000
+ */
